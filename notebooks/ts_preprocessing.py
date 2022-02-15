fe586f7c9de2d118bf8b82337444c457937cbc3a
Audrey Yin <ay2376@ad.nyu.edu>
2022-02-15 12:10:59 -0500

    Create new timestamp preprocessing file

---
+++ notebooks/ts_preprocessing.py
@@ -0,0 +1,9 @@
+################################################################################
+# Filename: ts_preprocessing.py
+# Description: Preprocessing bonsai, arduino, and video datafiles to extract cs
+#              ttls and calculate framerate
+# Author: Audrey Yin, ay2376@nyu.edu
+# Created On: 2022-02-15 10:57:54
+# Last Modified Date:
+# Last Modified By:
+################################################################################
