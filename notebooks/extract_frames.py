c9e4e0b78e5b3a8ade802f80ccc9d87963d08792
Audrey Yin <ay2376@nyu.edu>
2022-02-17 16:19:52 -0500

    Create extract_frames.py

---
+++ notebooks/extract_frames.py
@@ -0,0 +1,18 @@
+################################################################################
+# Filename: extract_frames.py
+# Description: Extracts frames from .avi given frame start and end indices.
+# Outputs:
+# Author: Audrey Yin, ay2376@nyu.edu
+# Created On: 2022-02-17 16:17:43
+# Last Modified Date:
+# Last Modified By:
+################################################################################
+
+# import modules
+import pandas as pd
+import numpy as np
+import os
+import sys
+import csv
+import ffmpeg
+import cv2
